12c708c3a9253956a6190779e422fc08a581ff2e
anuragrathor <45478290+anuragrathor@users.noreply.github.com>
2022-08-25 18:48:29 +0530

    nodejs

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const mongoose = require("mongoose");
+
+const userRoute = require("./routes/user");   
+const authRoute = require("./routes/auth");
+
+
+mongoose.connect(
+    process.env.MONGO_URL
+    ).then(()=> console.log("DB Connection Succesful"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.use(express.json()); 
+
+
+app.use("/api/users", userRoute);    
+app.use("/api/auth", authRoute);    
+
+    
+
+app.listen(process.env.PORT || 5000 , ()=>{
+    console.log("Backend server is running");
+});
