f54f5106d35d7de4726f96848c9289a0fd561471
anuragrathor <45478290+anuragrathor@users.noreply.github.com>
2022-08-25 18:53:44 +0530

    xzc

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const cryptoJs = require("crypto-js");
 
 //REGISTER USER
 router.post("/register", async (req, res) => {
